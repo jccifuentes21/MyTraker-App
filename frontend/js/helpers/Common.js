736c1412e5eba7496faf90b0e23563ee461b40f7
Juan Camilo <jccifuentes21@gmail.com>
2022-07-04 20:24:02 -0700

    can add users and check for existing users

--- frontend/js/helpers/Common.js
+++ frontend/js/helpers/Common.js
@@ -1,1 +1,55 @@
 //common functions that can be used in different cases
+
+export const postMethod = (endpoint, data) => {
+  switch (endpoint) {
+    case "accounts":
+      const newAccount = data;
+      $.ajax({
+        type: "POST",
+        url: `http://localhost:3000/${endpoint}`,
+        data: JSON.stringify({ newAccount }),
+        dataType: "json",
+        contentType: "application/json",
+      });
+      break;
+
+    case "transactions":
+      const newTransaction = data;
+      $.ajax({
+        type: "POST",
+        url: `http://localhost:3000/${endpoint}`,
+        data: JSON.stringify({ newTransaction }),
+        dataType: "json",
+        contentType: "application/json",
+      });
+      break;
+
+    case "categories":
+      const newCategory = data;
+      $.ajax({
+        type: "POST",
+        url: `http://localhost:3000/${endpoint}`,
+        data: JSON.stringify({ newCategory }),
+        dataType: "json",
+        contentType: "application/json",
+      });
+      break;
+  }
+};
+
+export const addNewAccount = (array, text) => {
+
+    $.get("http://localhost:3000/accounts", (data) => {
+      data.forEach((element) => {
+        array.push(element.username);
+      });
+
+      if (array.includes(text.val())) {
+        console.log("username already exists! Perform validation");
+      } else {
+        const newAccount = new Account(text.val());
+        postMethod("accounts", newAccount);
+      }
+    });
+};
+
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,18 @@
+import { addNewAccount, postMethod } from "./helpers/Common.js";
+
 $(() => {
   //Start coding here!
+  const newAccountForm = $("#newAccount-form");
+  const newAccountText = $("#newAccount");
+  
+  let usernamesArr = [];
+  $.get("http://localhost:3000/accounts", (data) => {
+    usernamesArr = data
+  });
+  
+  newAccountForm.click((e) => {
+    e.preventDefault();
+    
+    addNewAccount(usernamesArr, newAccountText);
+  });
 });
